f9aa3dfa37a4ff11307da0dd053cc33e34593772
SamKettlewell <44443765+SamKettlewell@users.noreply.github.com>
2018-11-05 02:36:30 +0000

    Add files via upload

---
+++ Birthday Problem - Homework 1.r
@@ -0,0 +1,13 @@
+#The Birthday Problem - Homework 1 - Sam Kettlewell - 201098385
+
+#Note we must use a loop because R does not like big numbers combined with factorial.
+p <- function(n) {
+  prob <- 1
+  for(x in 365:(365-n+1)) {
+    prob <- prob * (x/365)
+  }
+  1 - prob
+}
+
+#Print the solution corresponding to n=23.
+print(p(23))
